6d91591b79475b389a665be8a6d615ed4538a6dd
Caitlin <caitlin.colbert@gmail.com>
2014-09-19 22:38:43 -0700

    Updating flask yummly

--- yummly/static/js/main.js
+++ yummly/static/js/main.js
@@ -1,20 +1,41 @@
 $(function() {
     console.log("dom is ready!");
-   //  $('#post-form').on('submit', function(event){
+
+    $(".retry").hide()
+    $(".new-link").hide()
+
+
+    $('#post-form').on('submit', function(event){
    //      event.PreventDefault;
-   //      console.log("yay!")
+           console.log("yay!")
+           value = $('input[name="ingredient"]').val();
+           console.log(value)
    //      $('#top').hide()
-   //      $.ajax({
-   //          type: "POST",
-   //          url: "/",
-   //          data : { ingredient_list : $('.ingredients').val() },
-   //          success: function(result) {
-   //              console.log(result);
-   //          },
-   //          error: function() {
-   //              console.log("yikes")
-   //          }
-   //      });
-   // });
-});
+         $.ajax({
+             type: "POST",
+             url: "/",
+             data : { ingredient_list : value },
+             success: function(recipe) {
+                console.log(recipe);
+                $(".boom").hide()
+                $(".retry").show()
+                $(".new-link").show()
+                // $("#results").html(recipe.recipe_id)
+                $("#results").html('<h3><a href="http://www.yummly.com/recipe/'+recipe.recipe_id+'">'+
+                    recipe.recipe_name+'</a></h3><br><a href="http://www.yummly.com/recipe/'+recipe.recipe_id+
+                    '"><img src='+recipe.recipe_pic+' alt="Recipe photo" style="border-radius:50%;"></a><br><br>');
+            },
+            error: function(error) {
+                console.log(error)
+            }
+        });
+    });
+    $('.new-link').on('click', function(event){
+        event.preventDefault();
+        $("input").val('').show();
+        $(".boom").show()
+        $(".retry").hide()
+        $(".new-link").hide()
+        $('#results').html('');
+    });
 });
--- yummly/views.py
+++ yummly/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 
 from yummly import app
 from yummly import api 
@@ -16,24 +16,26 @@ def index():
 
     if request.method == "POST":
         errors = [] 
-        
-        ingredient_list = request.form['ingredient']  
-        single_recipe = []
+        result = []
+        ingredient_list = request.form.get('ingredient_list') 
 
         try:
             response = api.get_ingredients(ingredient_list) 
             single_recipe = random.choice(response["matches"])
-
+            result = {
+                "recipe_id": single_recipe["id"],
+                "recipe_name": single_recipe["recipeName"],
+                "recipe_pic": single_recipe['imageUrlsBySize']['90'].replace(
+                    's90-c', 's230-c')
+            }
+            code = 200
 
         except: # silencing all errors
-            #if not single_recipe:
-            errors.append("No results! Try again.")
+            result = {"sorry": "Sorry, no results! Please try again."}
+            code = 404
 
-        return render_template(
-            "index.html", 
-            recipe=single_recipe,
-            recipes=response,
-            errors=errors)
+
+        return jsonify(result)
 
     else:
 
