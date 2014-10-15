748cfddd36e4e1bb656d70eb8f81a0f8b41c0fe7
Caitlin <caitlin.colbert@gmail.com>
2014-10-15 08:41:30 -0700

    debugged database issues

--- create_db.py
+++ create_db.py
@@ -4,40 +4,9 @@ from yummly.models import User
 db.create_all()
 
 # insert data
-db.session.add(User("admin", "ad@min.com", "admin"))
-db.session.commit()
-
-<<<<<<< HEAD
-# sql.py - Create a SQLite3 table and populate it with data
-
-
-# import sqlite3
-
-# create a new database if the database doesn't already exist
-# with sqlite3.connect('sample.db') as connection:
-
-#     # get a cursor object used to execute SQL commands
-#     c = connection.cursor()
-
-#     # create the table
-#     c.execute('CREATE TABLE User(username TEXT, email TEXT, password TEXT)')
-
-    # insert dummy data into the table
-    # c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
-
-
-
-# db.create_all()
 # db.session.add(User("admin", "ad@min.com", "admin"))
 # db.session.commit()
 
-
-
-
-admin = User.query.filter(User.username=='test').first()
-print admin.email
-=======
 # sanity check!
 admin = User.query.filter_by(username='admin').first()
 print admin.username
->>>>>>> 9e32403591504d8ef167b12515423903392c74a7
