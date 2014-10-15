469ebbf6959afae12e6ca7874fdd9d1ea04a6a07
Michael Herman <hermanmu@gmail.com>
2014-10-15 06:46:54 -0500

    debugged database issues

--- yummly/api.py
+++ yummly/api.py
@@ -2,7 +2,6 @@
 
 
 import requests
-import json
 from secret import APP_ID, APP_KEY
 
 
@@ -10,13 +9,10 @@ def get_ingredients(ingredient_list):
     res = requests.get(
         "http://api.yummly.com/v1/api/recipes",
         params={
-            '_app_id': APP_ID, 
-            '_app_key': APP_KEY, 
-            'q': ingredient_list, 
+            '_app_id': APP_ID,
+            '_app_key': APP_KEY,
+            'q': ingredient_list,
             'maxResult': 100
         }
     )
     return res.json()
-
-
-
--- yummly/config.py
+++ yummly/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite:////flask-yummly/sample.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///yummly_test.db"
     DEBUG = True
     SECRET_KEY = "not-a-good-secret"
