122d73a9246a6ac9702eb3f478562edec1ddd23f
Caitlin <caitlin.colbert@gmail.com>
2014-12-02 08:49:42 -0800

    updating database

--- create_db.py
+++ create_db.py
@@ -4,23 +4,23 @@ from yummly.models import User, Recipe
 db.create_all()
 
 # insert data
-# db.session.add(User("admin", "admin", "admin"))
-# db.session.add(User("user", "user", "admin2"))
-# db.session.add(Recipe(
-#     "Pizza Bianca",
-#     "http://www.yummly.com/recipe/Pizza-Bianca-with-Anchovies-_-Kale-633201",
-#     "3",
-#     "pic",
-#     "ingredients")
-# )
-# db.session.commit()
+db.session.add(User("admin", "admin", "admin"))
+db.session.add(User("user", "user", "admin2"))
+db.session.add(Recipe(
+    "Pizza Bianca",
+    "http://www.yummly.com/recipe/Pizza-Bianca-with-Anchovies-_-Kale-633201",
+    "3",
+    "pic",
+    "ingredients")
+)
+db.session.commit()
 
 # sanity check!
-admin = User.query.filter_by(username='admin').first()
-print admin.password
+# admin = User.query.filter_by(username='admin').first()
+# print admin.password
 
-user = User.query.filter_by(username='user').first()
-print user.password
+# user = User.query.filter_by(username='user').first()
+# print user.password
 
-recipe = Recipe.query.filter_by().all()
-print recipe
+# recipe = Recipe.query.filter_by().all()
+# print recipe
