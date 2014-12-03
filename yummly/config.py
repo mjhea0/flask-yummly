e9b9107fe5f90cb229f964811d474a735c92fd08
Caitlin <caitlin.colbert@gmail.com>
2014-12-02 19:36:55 -0800

    database updates

--- yummly/config.py
+++ yummly/config.py
@@ -1,4 +1,12 @@
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///yummly_test.db"
+class BaseConfig(object):
     DEBUG = True
     SECRET_KEY = "not-a-good-secret"
+
+
+class DevelopmentConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///yummly_test.db"
+
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = "postgres://zgaejodwfynffw:cF8w21iRoZ9cpKfmwvFFisksk_@ec2-54-83-199-115.compute-1.amazonaws.com:5432/dairfoj5de3au0"
+    DEBUG = False
