3d1a3d8465a03a8f38a4d29c5aa5dae8df4969da
Michael Herman <hermanmu@gmail.com>
2015-01-05 08:36:50 -0700

    pep8 updates:

---
+++ yummly/secret_sample.py
@@ -0,0 +1,6 @@
+APP_ID = 'VALUE'
+APP_KEY = 'VALUE'
+
+# Find these values at https://twilio.com/user/account
+SID = 'VALUE'
+TOKEN = 'VALUE'
--- yummly/send_sms.py
+++ yummly/send_sms.py
@@ -1,13 +1,16 @@
 # Download the twilio-python library from http://twilio.com/docs/libraries
 from twilio.rest import TwilioRestClient
 
-from secret import sid, token
+from secret import SID, TOKEN
 
-# Find these values at https://twilio.com/user/account
-account_sid = account_sid
-auth_token = auth_token
-client = TwilioRestClient(account_sid, auth_token)
+client = TwilioRestClient(SID, TOKEN)
 
-message = client.messages.create(to="+19413200462", from_="+19419607434",
-                                     body="Hello there!",
-                                     media_url=['https://demo.twilio.com/owl.png', 'https://demo.twilio.com/logo.png'])
+message = client.messages.create(
+    to="+19413200462",
+    from_="+19419607434",
+    body="Hello there!",
+    media_url=[
+        'https://demo.twilio.com/owl.png',
+        'https://demo.twilio.com/logo.png'
+    ]
+)
